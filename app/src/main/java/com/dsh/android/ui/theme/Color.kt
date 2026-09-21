87226cb5dcc5110556a0eb788c26cd5f44a94eb7
噬言 <164013495+shiyanYX@users.noreply.github.com>
2026-09-21 21:17:08 +0800

    feat: add Material 3 theme and string resources

---
+++ app/src/main/java/com/dsh/android/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.dsh.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue80 = Color(0xFFBBDEFB)
+val BlueGrey80 = Color(0xFFCFD8DC)
+val Teal80 = Color(0xFFB2DFDB)
+
+val Blue40 = Color(0xFF1976D2)
+val BlueGrey40 = Color(0xFF546E7A)
+val Teal40 = Color(0xFF00796B)
+
+val SurfaceDark = Color(0xFF1E1E1E)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceLight = Color(0xFFFAFAFA)
+val BackgroundLight = Color(0xFFF5F5F5)
